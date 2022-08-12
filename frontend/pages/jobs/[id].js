49e49c8179017bca145b57d03bc11a0c9c0689a0
TreciaKS <tksekgalo@gmail.com>
2022-08-12 13:27:58 +0200

    Styled detailed job page

--- frontend/pages/jobs/[id].js
+++ frontend/pages/jobs/[id].js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export const getStaticPaths = async () => {
     const res = await fetch("http://localhost:1337/api/jobs?populate=*")
 
@@ -58,13 +60,49 @@ export const getStaticProps = async ({params}) => {
 }
 
 const DetailedJobs = ({job}) => {
+    const Max_DATE_LENGHT = 10
+    const Max_TIME_LENGHT = 11
+    
     return ( 
         <>
-            <h2 className="mb-2 text-xl font-bold leading-snug text-gray-900">
+        {/* {JSON.stringify(job, null, 2)} */}
+        <article className="px-4 py-5 mx-auto max-w-7xl" itemid="#" itemscope itemtype="http://schema.org/BlogPosting">
+            <div className="w-full mx-auto mb-12 text-left md:w-3/4 lg:w-1/2">
+                <p className="mt-6 mb-2 text-xs font-semibold tracking-wider uppercase text-primary">{job.attributes.categories.data[0].attributes.type + " LEVEL"}</p>
+                <h1 className="mb-3 text-3xl font-bold leading-tight text-gray-900 md:text-4xl">
                 {job.attributes.vacancy}
-                {/* {JSON.stringify(job, null, 2)} */}
+                </h1>
+                <div className="flex mb-6 space-x-2 text-sm">
+                <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">CSS</a>
+                <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">Tailwind</a>
+                <a className="p-1 bg-indigo-500 rounded-full text-gray-50 badge hover:bg-gray-200" href="#">AlpineJS</a>
+                </div>
+                <a className="flex items-center text-gray-700" href="#">
+                <div className="avatar">
+                    <img 
+                    className="flex-shrink-0 object-cover object-center w-24 h-24 rounded-full"
+                    src={`http://localhost:1337${job.attributes.image.data.attributes.url}`}
+                    alt={"Photo of " + job.attributes.recruiter} />
+                </div>
+                <div className="ml-4">
+                    <p className="font-semibold text-gray-800 text-md">{job.attributes.recruiter}</p>
+                    <p className="text-sm text-gray-500">{job.attributes.publishedAt.substring(0, Max_DATE_LENGHT)}</p>
+                    <p className="text-sm text-gray-500">{job.attributes.createdAt.substring(16, Max_TIME_LENGHT)}</p>
+                </div>
+                </a>
+            </div>
 
-            </h2>
+            <div className="w-full mx-auto prose md:w-3/4 lg:w-1/2">
+                <p>
+                {job.attributes.description}
+                </p>
+            </div>
+            
+            <div className="flex flex-col items-center justify-center mt-10 space-x-0 space-y-2 md:space-x-2 md:space-y-0 md:flex-row">
+                 <Link href="/jobs"><a href="#" className="px-3 py-2 text-indigo-500 border border-indigo-500 border-solid hover:text-black md:w-auto">Back</a>
+                 </Link>
+             </div>
+        </article>
         </>
      )
 }
