b8e593552ac3ce593e4027491072638e241fe52b
TreciaKS <tksekgalo@gmail.com>
2022-08-15 17:45:35 +0200

    updated .env

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,8 +1,39 @@
+// path: ./config/middlewares.js
+
 module.exports = [
+  // The array is pre-populated with internal, built-in middlewares, prefixed by `strapi::`
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
+
+  // custom middleware that does not require any configuration
+  'my-custom-node-module', 
+
+  // custom name to find a package or a path
+  {
+    name: 'my-custom-node-module',
+    config: {
+      foo: 'bar',
+    },
+  },
+
+  // custom resolve to find a package or a path
+  {
+    resolve: '../some-dir/custom-middleware',
+    config: {
+      foo: 'bar',
+    },
+  },
+
+  // custom configuration for internal middleware
+  {
+    name: 'strapi::poweredBy',
+    config: {
+      poweredBy: 'Some awesome company',
+    },
+  },
+
+  // remaining internal & built-in middlewares
   'strapi::logger',
   'strapi::query',
   'strapi::body',
