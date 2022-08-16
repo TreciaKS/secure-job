85e620c8569b549cded00610a02783445c119e26
TreciaKS <tksekgalo@gmail.com>
2022-08-16 14:53:34 +0200

    updated backend

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,21 +1,19 @@
-const parse = require('pg-connection-string').parse
-const config = parse(process.env.DATABASE_URL)
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 
-// path: ./config/database.js
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
       },
-      debug: false,
     },
-  });
+    debug: false,
+  },
+});
