91ad26d59baa12dfb49a1c7c92684c098b2886cc
TreciaKS <tksekgalo@gmail.com>
2022-09-12 12:10:17 +0200

    sessionprovider added

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import { SessionProvider } from 'next-auth/react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider>
       <Head>
         <title>Secure Job</title>
         <meta name="description" content="Job vacancies at your disposal" />
@@ -13,7 +14,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   )
 }
 
